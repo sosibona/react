20db61bb09fb1264bae53992521d4318681fb0ef
sosibona <empty.tara@gmail.com>
2020-03-25 16:07:20 +0200

    create app

---
+++ lesson1/hw1/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const rootElement = document.querySelector('#root');
+
+const createElement = <div className="title">Todo List</div>;
+
+ReactDOM.render(createElement, rootElement);
+
+
