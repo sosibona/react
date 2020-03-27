27012bdacab69f43aefbe95fead14d4de114abd3
sosibona <empty.tara@gmail.com>
2020-03-27 16:38:12 +0200

    sol3

--- lesson3/hw2/src/Profile.jsx
+++ lesson3/hw2/src/Profile.jsx
@@ -4,7 +4,7 @@ import getBirthDate from './getBirthDate';
 const Profile = (props) => {
   return (
     <>
-      <h1 className="profile__name">{props.user.firstName} {props.user.lastName}</h1>
+      <div className="profile__name">{props.user.firstName} {props.user.lastName}</div>
       <div className="profile__birth">Was born {getBirthDate(props.user.birthDate)} in {props.user.birthPlace}</div>
     </>
   );
