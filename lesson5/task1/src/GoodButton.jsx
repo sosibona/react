327d3f2dc1375c9515c57379cc9c138bed5666d1
sosibona <empty.tara@gmail.com>
2020-03-29 01:19:39 +0200

    sol

---
+++ lesson5/task1/src/GoodButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const GoodButton = () => {
+  return (
+    <button className="fancy-button"
+      onClick={() => {
+        alert('Good Job!');
+      }}
+    >
+      Click me!
+    </button>
+  )
+};
+
+export default GoodButton;
---
+++ lesson5/task1/src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import GoodButton from './GoodButton'
+
+const root = document.querySelector('#root')
+
+ReactDOM.render(
+  <GoodButton />,
+  root
+);
+
