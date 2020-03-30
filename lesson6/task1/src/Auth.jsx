94f91f87dbac4e65bf1744b9eceaba7756bf057b
sosibona <empty.tara@gmail.com>
2020-03-30 22:29:20 +0300

    sol

---
+++ lesson6/task1/src/Auth.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Greeting from './Greeting';
+
+
+class Auth extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    }
+  }
+
+  handleLogin = () => {
+    this.setState({
+      isLoggedIn: true
+    })
+  }
+
+  handleLogout = () => {
+    this.setState({
+      isLoggedIn: false
+    })
+  }
+
+  render() {
+
+    const button = this.state.isLoggedIn
+      ? <button onClick={this.handleLogout}>Logout</button>
+      : <button onClick={this.handleLogin}>Login</button>;
+
+    // if (this.state.isLoggedIn) {
+    //   button = <button onClick={this.handleLogout}>Logout</button>;
+    // } else {
+    //   button = <button onClick={this.handleLogin}>Login</button>;
+    // }
+
+    return (
+      <div className="panel">
+        <Greeting isLoggedIn={this.state.isLoggedIn}/>
+        {this.state.isLoggedIn
+          ? <button onClick={this.handleLogout}>Logout</button>
+          : <button onClick={this.handleLogin}>Login</button>}
+      </div>
+    )
+  }
+}
+
+export default Auth;
---
+++ lesson6/task1/src/UserGreeting.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UserGreeting = () => {
+  return (
+    <h1>Hello, user 😎</h1>
+  )
+}
+
+export default UserGreeting;
