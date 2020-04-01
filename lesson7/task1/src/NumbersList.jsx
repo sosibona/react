c85e77a623e8a9791f9692ea00eccf0de8a52c36
sosibona <empty.tara@gmail.com>
2020-04-01 17:31:30 +0300

    fix name

--- lesson7/task1/src/NumbersList.jsx
+++ lesson7/task1/src/NumbersList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const NumberList = ({ numbers }) => {
+const NumbersList = ({ numbers }) => {
   const numberList = numbers.map(num => {
   return <li key={num}>{num}</li>
   })
@@ -11,4 +11,4 @@ const NumberList = ({ numbers }) => {
   )
 }
 
-export default NumberList;
+export default NumbersList;
--- lesson7/task1/src/index.jsx
+++ lesson7/task1/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import NumberList from './NumberList'
+import NumbersList from './NumbersList'
 import './index.scss'
 
 const numbers = [1, 2, 3, 4];
@@ -8,6 +8,6 @@ const numbers = [1, 2, 3, 4];
 const root = document.querySelector('#root');
 
 ReactDOM.render(
-  <NumberList numbers={numbers}/>, 
+  <NumbersList numbers={numbers}/>, 
   root
 )
