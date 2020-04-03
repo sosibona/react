d5863717ed058296f7789e9ea1f83f821b469266
sosibona <empty.tara@gmail.com>
2020-04-03 23:41:18 +0300

    form

---
+++ lesson9/task2/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import UserForm from "./UserForm";
+import "./index.scss";
+
+const root = document.querySelector("#root");
+
+ReactDOM.render(<UserForm />, root);
