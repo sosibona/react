7a95793c93b3d97218d95dd961ad3494fc053437
sosibona <empty.tara@gmail.com>
2020-04-03 15:45:04 +0300

    fetch

---
+++ lesson8/task4/src/User.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+class User extends Component {
+  state = {
+    user: null
+  };
+
+  componentDidMount() {
+    this.fetchUser();
+  }
+
+  fetchUser = () => {
+    fetch(`https://api.github.com/users/${this.props.user}`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          user: data
+        });
+      });
+  };
+
+  render() {
+    if (!this.state.user) return null;
+    return (
+      <div className="user">
+        <img
+          alt="User Avatar"
+          src="https://avatars3.githubusercontent.com/u/69631?v=4"
+          src={this.state.user.avatar_url}
+          className="user__avatar"
+        />
+        <div className="user__info">
+          <span className="user__name">{this.state.user.name}</span>
+          <span className="user__location">{this.state.user.location}</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default User;
---
+++ lesson8/task4/src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import User from "./User";
+import "./index.scss";
+
+
+const root = document.querySelector("#root");
+
+ReactDOM.render(<User user="facebook"/>, root);
