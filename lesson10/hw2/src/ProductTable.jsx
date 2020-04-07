d7dfaab2f83a78ba888b02e222f22b09923f5e81
sosibona <empty.tara@gmail.com>
2020-04-07 22:53:30 +0300

    product card

--- lesson10/hw2/src/ProductTable.jsx
+++ lesson10/hw2/src/ProductTable.jsx
@@ -5,30 +5,27 @@ import ProductRow from "./ProductRow";
 const ProductTable = ({ products, filterText, inStockOnly }) => {
   const rows = [];
   let lastCategory = null;
-  let searchProducts;
-
-  if (inStockOnly) {
-    searchProducts = products.filter((product) => {
-      return (
-        product.stocked === inStockOnly && product.name.includes(filterText)
-      );
-    });
-  } else {
-    searchProducts = products.filter((product) => {
-      return product.name.includes(filterText);
-    });
-  }
-
-  searchProducts.forEach((product) => {
+  
+  products.forEach((product) => {
+    if (product.name.indexOf(filterText) === -1) {
+      return;
+    }
+    if (inStockOnly && !product.stocked) {
+      return;
+    }
     if (product.category !== lastCategory) {
       rows.push(
         <ProductCategoryRow
           category={product.category}
-          key={product.category}
-        />
+          key={product.category} />
       );
     }
-    rows.push(<ProductRow product={product} key={product.name} />);
+    rows.push(
+      <ProductRow
+        product={product}
+        key={product.name}
+      />
+    );
     lastCategory = product.category;
   });
 
