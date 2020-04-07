422f29d0eb03a67b324cfe2cb2a9d101fa808a24
sosibona <empty.tara@gmail.com>
2020-04-07 13:25:30 +0300

    fix

--- lesson10/task2/src/App.jsx
+++ lesson10/task2/src/App.jsx
@@ -26,7 +26,7 @@ class Page extends Component {
       <div className="page">
         <h1 className="title">Hello, {userData.firstName} {userData.lastName}</h1>
         <main className="content">  
-          <ShoppingCart userData={this.state.userData}/>
+          <ShoppingCart userName={this.state.userData.firstName}/>
           <Profile userData={this.state.userData} handleChange={this.handleChange}/>
         </main>
       </div>
